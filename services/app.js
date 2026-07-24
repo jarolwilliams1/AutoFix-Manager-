f73dd33d08f5dc47123929a1277f98ebf5fb6bcf
JarolWilliams <215430519+jarolwilliams1@users.noreply.github.com.>
2026-07-24 00:06:17 -0400

    trabajando en la practica

---
+++ services/app.js
@@ -0,0 +1,8 @@
+const btnCrearMarca = document.getElementById("btn-crear-marca")
+const nuevaMarcaPantalla = document.getElementById("nueva-marca")
+btnCrearMarca.addEventListener("click", ()=>{
+
+    alert("hola")
+    nuevaMarcaPantalla.hidden = true;
+
+})
